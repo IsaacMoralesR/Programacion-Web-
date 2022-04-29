deab220db5a90e7fb8e7f4b41517244f485578e7
IsaacMoralesR <104519405+IsaacMoralesR@users.noreply.github.com>
2022-04-29 13:46:17 -0500

    archivo sesion bucles 2

---
+++ bucles dos/array.js
@@ -0,0 +1,21 @@
+
+// declaramos un array 
+//var i = 0;
+var pokemones = ["Pikachu ", "Bulbasaur ", "Charmander "];
+//ciclo while 
+//while (i <= 2){//codigo que se ejecuta 
+//console.log(pokemones[i]);
+//i++; 
+//}*/
+
+//Ciclo Do While, primero se declara Do, luego las llaves y despúes va "While" donde se comprueban las variables que se van a ejecutar
+//do{
+//console.log (pokemones[i]);
+//i++;
+//while (i <= 2);}
+
+//Ciclo For
+for (var i = 0 ; i<= 2 ; i++){
+    //codigo que se ejecuta
+   document.write(pokemones[i] + "yo te elijo <br>");
+} 
---
+++ bucles dos/ciclosp2.js
@@ -0,0 +1,22 @@
+/*
+Bucle For
+
+Determinado 
+
+Su sintaxis consta de tres partes:
+
+-variable
+-número de iteraciones
+-incrementación o decrementación 
+-las tres partes del Ciclo For deben ir separadas por ";"
+
+for (variable;No. de iteraciones incrementación/decrementación) {Código que se ejecuta}
+*/
+
+//for(var i = 0; i < 3; i++) {
+ //   console.log(i); }
+//ciclo for decrementar
+//()
+    for(var i = 10; i > 0; i--){
+        console.log(i);
+    } 
