2afd8246d69d6c9a877af1c5f35b34b781f5ed38
IsaacMoralesR <104519405+IsaacMoralesR@users.noreply.github.com>
2022-05-25 11:53:20 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,4 @@
+var x=1;
+var y=7;
+var z=x+y;
+alert("El valor de z es " + z );
---
+++ edad en el futuro clase/code.js
@@ -0,0 +1,9 @@
+var name = prompt("¿Cuál es tu nombre?");
+var edad = prompt("¿Cuál sería tu edad en este año?") ;
+var futuro = prompt("¿En qué año futuro quieres saber tu edad?");
+var actual = 2022;
+
+var nacimiento = actual - edad;
+var edadFuturo = futuro - nacimiento;
+
+console.log("Hola " + name + " naciste en el año " + nacimiento + " tu edad en el año " + futuro + " sería de " + edadFuturo + " años, y serás viejito");
