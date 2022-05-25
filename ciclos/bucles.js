b0970655fd309fb150ca6edf592a85ae05e08b1f
IsaacMoralesR <104519405+IsaacMoralesR@users.noreply.github.com>
2022-05-25 11:51:46 -0500

    Add files via upload

---
+++ ciclos/bucles.js
@@ -0,0 +1,26 @@
+ alert("Enlazado y funcionando");
+
+/* 
+ciclo while sirve para ejecutar un codigo de forma ciclica, mientras su condicion se cumpla}
+*/
+/* 
+Definimos la variable "i"
+*/
+//var i = 0;
+//var numero= prompt("Elige un numero");
+
+//while(i <=numero ) {
+    //Aquí va el código que se ejecuta
+  //  document.write("Hola, soy un ciclo.<br>");
+    //Sumamos una unidad a la variable "i" para que la condición deje de cumplirse
+  //  i++;                
+//}  */
+
+/* El ciclo Do While ejecuta un código al menos una vez y si se cumple una condición, volverá a repetirlo
+*/
+var i = 10; 
+
+do {
+//aquí el código que se ejecuta en el ciclo
+document.write("Isaac");
+} while ( i << 10 );
